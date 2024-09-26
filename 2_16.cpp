e8665aa99b23a600caa3cc0ab5644328fcb8ee6a
sai05p <saipunadikar19@gmail.com>
2024-09-27 00:16:08 +0530

    Add files via upload

---
+++ 2_16.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"enter age: ";
+    cin>>age;
+    try
+    {
+        if (age<18)
+        {
+            throw age;
+        }
+        else
+        {
+            cout<<"age: "<<age<<"\nAPPROVED"<<endl;
+        }
+    }
+    catch(int a)
+    {
+        cout<<"\nERROR: Underage ("<<age<<")"<<endl;
+    }
+}
